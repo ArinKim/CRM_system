e16c0db03881c7ecce0e5b4c680c3592ccb23123
ArinKim <63277906+ArinKim@users.noreply.github.com>
2025-03-29 21:54:33 +1000

    commit: add mock data

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,14 @@
+import { MOCK_PROJECTS } from "../data/projects";
+
 function HomePage() {
-    return <h1>Home</h1>;
+    return  (
+        <div className="home">
+            <h1 className="title">Welcome to the Home Page</h1>
+            <p className="description">This is a simple home page.</p>
+        <h1 className="projects">Projects</h1>
+        <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+        </div>
+    )
   }
   
   export default HomePage;
