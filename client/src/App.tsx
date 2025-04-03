9826ca9c3153aa52cab0b22e79d16c436cfc1ccc
ArinKim <63277906+ArinKim@users.noreply.github.com>
2025-04-03 21:25:26 +1000

    commit: Add top nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 import ItemsPage from "./pages/ItemsPage";
+import TopAppBar from "./components/AppBar/AppBar";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
+import UserPage from "./pages/UserPage";
 
 function App() {
   return (
-    <div className="container">
-      <ItemsPage />
-    </div>
+    <BrowserRouter>
+      <>
+        <div className="container">
+          <TopAppBar />
+          <Routes>
+            <Route path="/user" element={<UserPage />} />
+            <Route path="/items" element={<ItemsPage />} />
+          </Routes>
+        </div>
+      </>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/AppBar/AppBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { NavLink } from "react-router";
+
+function TopAppBar() {
+  return (
+    <header className="sticky flex justify-between items-center p-4 bg-gray-800 text-white shadow-md">
+      <NavLink to="/items" className="text-xl font-bold hover:text-gray-300">
+        MyLogo
+      </NavLink>
+      <NavLink to="/user" className="button rounded hover:text-gray-300">
+        <span className="text-4xl">
+          <FaUserCircle />
+        </span>
+      </NavLink>
+    </header>
+  );
+}
+
+export default TopAppBar;
