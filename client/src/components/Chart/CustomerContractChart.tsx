5656cfad864dd693859b0dd0d78642ca811ba0bb
ArinKim <63277906+ArinKim@users.noreply.github.com>
2025-04-03 21:48:09 +1000

    commit: Add charts for contract range and daily sales

---
+++ client/src/components/Chart/CustomerContractChart.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  [
+    { type: "string", id: "Term" },
+    { type: "string", id: "Name" },
+    { type: "date", id: "Start" },
+    { type: "date", id: "End" },
+  ],
+  ["1", "George", new Date(2024, 3, 30), new Date(2024, 6, 4)],
+  ["2", "John", new Date(2024, 2, 4), new Date(2025, 2, 4)],
+  ["3", "Thomas", new Date(2024, 11, 4), new Date(2025, 5, 4)],
+];
+
+// https://www.react-google-charts.com/examples/timeline
+function CustomerContractChart() {
+  return (
+    <div className="schedule-container">
+      <h1 className="schedule">Customer Schedule</h1>
+
+      <div className="schedule-description">
+        <p>
+          This chart shows the schedule of customers. The chart is a timeline
+          that displays the start and end dates of each customer's schedule.
+        </p>
+      </div>
+      <Chart chartType="Timeline" data={data} width="100%" height="400px" />
+    </div>
+  );
+}
+
+export default CustomerContractChart;
--- client/src/pages/ItemsPage.tsx
+++ client/src/pages/ItemsPage.tsx
@@ -1,6 +1,8 @@
 import { MOCK_ITEMS } from "../data/Items";
 import ProjectList from "../components/Items/ItemList";
 import { Item } from "../models/Item.model";
+import CustomerContractChart from "../components/Chart/CustomerContractChart";
+import DailySalesChart from "../components/Chart/DailySalesChart";
 
 function ProjectPage() {
   const saveProject = (items: Item) => {
@@ -11,6 +13,8 @@ function ProjectPage() {
     <div className="home">
       <h1 className="items">Items</h1>
       <ProjectList onSave={saveProject} items={MOCK_ITEMS} />
+      <DailySalesChart />
+      <CustomerContractChart />
     </div>
   );
 }
