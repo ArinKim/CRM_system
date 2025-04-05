df8b68247d8ad12f4f6afc7bea8bc0f3facabe23
ArinKim <63277906+ArinKim@users.noreply.github.com>
2025-04-06 06:38:31 +1000

    commit: code cleaning

--- server/models/customer/customers.ts
+++ server/models/customer/customers.ts
@@ -1,7 +1,5 @@
 import { User } from "../user/users";
 import { CustomerInterface } from "./customer.interface";
-import { User } from "../user/users";
-import { CustomerInterface } from "./customer.interface";
 
 export class Customer {
   public id: string;
