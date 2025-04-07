79bd26b94a8416ee0a7c1840ca098606a739da79
ArinKim <63277906+ArinKim@users.noreply.github.com>
2025-04-07 16:00:40 +1000

    commit: Return daily sales chart data

--- client/src/components/Chart/DailySalesChart.tsx
+++ client/src/components/Chart/DailySalesChart.tsx
@@ -3,15 +3,6 @@ import { Chart } from "react-google-charts";
 import { DailySalesChartAPI } from "./DailySalesChartAPI";
 import { Sales } from "../../models/sales/sales";
 
-const data = [
-  ["Customers", "AUD"],
-  ["A", 1100],
-  ["B", 2455],
-  ["C", 200],
-  ["D", 250],
-  ["E", 700],
-];
-
 const options = {
   title: "Daily Sales",
   pieHole: 0.2, // Creates a Donut Chart. Does not do anything when is3D is enabled
@@ -38,23 +29,12 @@ function DailySalesChart() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | undefined>(undefined);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:3300/api/sales/get-info/")
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setSales(data);
-  //     });
-  // }, []);
-
   useEffect(() => {
     async function loadSales() {
       setLoading(true);
       try {
         const data = await DailySalesChartAPI.get();
-        console.log(data);
+
         setError("");
         setSales(data);
       } catch (e) {
@@ -78,17 +58,12 @@ function DailySalesChart() {
           displays the sales of each customer.
         </p>
       </div>
-      <div>
-        {sales?.map((item, index) => (
-          <div key={index}>
-            <p>{item.id}</p>
-          </div>
-        ))}
-      </div>
-
       <Chart
         chartType="PieChart"
-        data={data}
+        data={[
+          ["Customers", "AUD"],
+          ...sales.map((item) => [item.customer.company, item.value.amount]),
+        ]}
         options={options}
         width="auto"
         height="400px"
